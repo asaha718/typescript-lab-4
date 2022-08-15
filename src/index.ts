b2d6471c4c0127bea9791d3d6be0fcb77e6692c9
Anug Saha <anugsaha@gmail.com>
2022-08-15 14:34:23 -0400

    using generics and arrays

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-// your code here
+const button = document.querySelector("button") as HTMLButtonElement;
+const input1 = document.getElementById("spaces") as HTMLInputElement;
+const input2 = document.getElementById("word") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+  console.log(prepend([input1.value, input2.value]));
+});
+
+function prepend<T>(array : T[]) {
+    let spaces: string= ""; 
+    
+    for(let i=0; i< +array[0]; i++){ 
+      spaces += " "
+    }
+    
+    return spaces + +array[1]
+}
